20527f215df6750fac86423e09a593ecf57cd96c
crystalhwaang <chwang610@gmail.com>
2023-01-18 16:24:00 -0700

    Add project files.

---
+++ Mission2Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mission2Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int[] rollTotals = new int[6];
+
+            //Simulate the roll of the die
+            for (int i = 0; i < 100; i++)
+            {
+                int roll = r.Next(6);
+                rollTotals[roll]++;
+            }
+            rollTotals[1] = r.Next(6) + 1;
+
+            //Print out the results from the array
+            for (int = int; i < 6; ++i)
+            {x
+                Console.WriteLine((i+1) + ": " + rollTotals[i]);
+            }
+        }
+    }
+}
